598f532029e7da95eb113a110ec5a10aee08a610
Marlon Raphael <raphael27atm@gmail.com>
2020-07-23 10:11:19 -0300

    Implement query params

--- app/resources/author_resource.rb
+++ app/resources/author_resource.rb
@@ -1,3 +1,18 @@
+# frozen_string_literal: true
+
+# This class serialize model Author
 class AuthorResource < JSONAPI::Resource
   attributes :first, :last
+
+  filters :query
+  
+  def self.apply_filter(records, filter, value, options)
+    case filter
+    when :query
+      records.where('last LIKE ?', "%#{value.first}%")
+             .or(records.where('first LIKE ?', "%#{value.first}%"))
+    else
+      super
+    end
+  end
 end
